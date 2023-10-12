ac1fce8dc5837bd7c66a94a6327c4d101a389ce4
carniiga <fernandezagustin98@.com>
2023-10-12 16:05:34 -0300

    prisma generate start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import  { app, prismaConnect } from "./server/app.js";
+import  { app, prismaConnect} from "./server/app.js";
 import * as dotenv from "dotenv"
 import { userRouter } from "./server/routes/user-auth/user.routes.js";
 import { productRouter } from "./server/routes/products/products.routes.js";
@@ -7,12 +7,13 @@ import { productRouter } from "./server/routes/products/products.routes.js";
 dotenv.config()
 
 
+
+
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use("/", userRouter)
 app.use("/", productRouter)
 app.listen(port ,() => {
-    prismaConnect()
     console.log(`listening on port ${port}`)
     
     
--- src/server/app.js
+++ src/server/app.js
@@ -6,10 +6,11 @@ import express from "express"
 
 export const app = express()
 
-let prismaClient;
+
 export const prisma = new PrismaClient();
 
 export const prismaConnect = () => {
-    prismaClient = new PrismaClient
+     const connect = prismaClient = new PrismaClient()
+     return connect
 }
 
