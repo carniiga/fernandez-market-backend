20a1b755777c2f0342104b335ce09f6265635bb2
carniiga <fernandezagustin98@.com>
2023-10-14 15:32:43 -0300

    cors

--- src/server/app.js
+++ src/server/app.js
@@ -6,7 +6,10 @@ import express from "express"
 
 export const app = express()
 
-
+app.use(cors({
+     origin: '*'
+ }));
+ 
 export const prisma = new PrismaClient();
 
 export const prismaConnect = () => {
